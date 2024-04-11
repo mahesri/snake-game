454a0e9be9748d1978daeaaf80a607959738fa1e
mahesri <adi504097@gmail.com>
2024-04-11 21:28:55 +0700

    add rource code

--- src/com/mahesri/Board.java
+++ src/com/mahesri/Board.java
@@ -4,7 +4,9 @@ import java.util.Random;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener; 
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent; 
 
 
 public class Board extends JPanel implements ActionListener {
@@ -18,9 +20,12 @@ public class Board extends JPanel implements ActionListener {
     final int x[] = new int[gameUnits];
     final int y[] = new int[gameUnits];
 
-    int bodyParts = 6; 
+    int bodyParts = 6;
+    int appleEaten;
 
     boolean running = false;
+
+    char direction = 'R';
 
     int appleX;
     int appleY;
@@ -37,6 +42,8 @@ public class Board extends JPanel implements ActionListener {
       random = new Random();
       this.setPreferredSize(new Dimension(borderWidth, borderHeight));
       this.setBackground(new Color(333333));
+      this.addKeyListener(new MyKeyAdapter());
+      this.setFocusable(true);
       startGame();
       loadImage();
     }
@@ -67,24 +74,116 @@ public class Board extends JPanel implements ActionListener {
     }
 
     public void doDraw(Graphics g){
-      
+
 
     if(running){
     g.drawImage(apple, appleX, appleY, unitsSize, unitsSize, null);
     
+      for(int i = 0; i < bodyParts;i++){
+        
+        if(i == 0){
+          if(direction == 'R' || direction == 'L'){
+            g.drawImage(headY, x[i], y[i], unitsSize, unitsSize, null);
+          }else{
+            g.drawImage(headX, x[i], y[i], unitsSize, unitsSize, null);
+          }
+
+        }else{                   
+          g.setColor(new Color(random.nextInt(225),random.nextInt(255),random.nextInt(255),random.nextInt(255)));
+          g.fillOval(x[i], y[i], unitsSize, unitsSize);
+        }
+      }
+      g.setColor(Color.white);
+      g.setFont(new Font("Waltograph UI",Font.BOLD, 20));
+      FontMetrics metrics = getFontMetrics(g.getFont());
+      g.drawString("Score : "+appleEaten, (borderWidth - metrics.stringWidth("Score : "+appleEaten))/2, borderHeight/7);
+
+    }else{
+      gameOver(g);
     }
 
     }
+
 
     public void locateApple(){
       appleX = random.nextInt((int)(borderWidth/unitsSize))*unitsSize;
       appleY = random.nextInt((int)(borderHeight/unitsSize))*unitsSize;
     }
 
+    public void move(){
+
+      for(int i = bodyParts; i > 0; i--){
+        x[i] = x[i-1];
+        y[i] = y[i-1];
+      }
+
+      switch (direction) {
+        case 'U':
+          y[0] = y[0] - unitsSize;
+          break;
+          case 'D':
+          y[0] = y[0] + unitsSize;
+          break;
+          case 'L':
+          x[0] = x[0] - unitsSize;
+          break;
+          case 'R':
+          x[0] = x[0] + unitsSize;
+          break;
+ 
+      }
+
+
+    }
+
     public void checkApple(){
       if((x[0] == appleX)&& (y[0]== appleY)){
-
+        bodyParts++;
+        appleEaten++;
+        locateApple();
       }
+    }
+
+    public void checkCollisions() {
+
+      for(int i = bodyParts; i > 0; i--){
+        if((x[0] == x[i])&&(y[0] == y[i])){
+          running = false;
+        }
+      }
+
+      if(x[0] < 0){
+        running = false;
+      }
+
+      if(x[0] >= borderWidth){
+        running = false;
+      }
+
+      if(y[0] < 0){
+        running = false;
+      }
+
+      if(y[0]>=borderHeight){
+        running = false;
+      }
+
+      if(!running){
+        timer.stop();
+      }
+    }
+
+    public void gameOver(Graphics g){
+
+      g.setColor(new Color(115, 255, 65));
+        g.setFont(new Font("Waltograph UI",Font.BOLD, 50));
+        FontMetrics metrics1 = getFontMetrics(g.getFont());
+        g.drawString("Game Over!", (borderWidth - metrics1.stringWidth("Game Over!"))/2 , borderHeight /5);
+
+        g.setColor(Color.white);
+        g.setFont(new Font("Waltograph UI",Font.BOLD, 20));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("Score : "+appleEaten, (borderWidth - metrics2.stringWidth("Score"+appleEaten))/2, borderHeight/3);
     }
 
     @Override
@@ -92,6 +191,40 @@ public class Board extends JPanel implements ActionListener {
 
       if(running){
         checkApple();
+        move();
+        checkCollisions();
       }
       repaint();
-    }}
+    }
+  
+  public class  MyKeyAdapter extends KeyAdapter {
+  
+    @Override
+    public void keyPressed(KeyEvent e){
+      switch (e.getKeyCode()) {
+        case KeyEvent.VK_LEFT:
+        if(direction != 'R'){
+          direction = 'L';
+        }
+          break;
+        case KeyEvent.VK_RIGHT:
+        if(direction != 'L'){
+          direction = 'R';
+        }
+          break;
+          case KeyEvent.VK_UP:
+          if(direction != 'D'){
+            direction = 'U';
+          }
+            break;
+            case KeyEvent.VK_DOWN:
+            if(direction != 'U'){
+              direction = 'D';
+            }
+              break;  
+      
+      }
+    }
+    
+  }}
+
